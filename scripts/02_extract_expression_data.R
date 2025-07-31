1119c6da3b1ff8847dbd0dde745c86ecc41e8d1e
mbcxjrw4 <xiruowang@hotmail.com>
2025-07-31 15:53:27 +0100

    Update 02_extract_expression_data.R for shiny app

--- scripts/02_extract_expression_data.R
+++ scripts/02_extract_expression_data.R
@@ -1,60 +1,56 @@
-# tsv.gz file of gene id and name is brought into R.
-UCSC_Path <- "data/UCSC/combined/"
-file4 <- "TOIL-GTEX_TARGET_TCGA-ROW_DATA.tsv.gz"
-gene <- data.table::fread(file = paste0(UCSC_Path, file4), check.names=FALSE, stringsAsFactors=F)
+extract_expression_data <- function(membrane_targets){
+    # 1. load data
+    # tsv.gz file of gene id and name is brought into R.
+    UCSC_Path <- "data/UCSC/"
+    file4 <- "TOIL-GTEX_TARGET_TCGA-ROW_DATA.tsv.gz"
+    gene <- data.table::fread(file = paste0(UCSC_Path, file4), check.names=FALSE, stringsAsFactors=F)
 
-# TSV file of meta data is brought into R.
-file5 <-  "TOIL-GTEX_TARGET_TCGA-COLUMN_DATA.tsv"
-meta <- data.table::fread(file=paste0(UCSC_Path, file5), check.names=FALSE, stringsAsFactors=F)
+    # TSV file of meta data is brought into R.
+    file5 <-  "TOIL-GTEX_TARGET_TCGA-COLUMN_DATA.tsv"
+    meta <- data.table::fread(file=paste0(UCSC_Path, file5), check.names=FALSE, stringsAsFactors=F)
 
-# TSV file of integrated RNAseq data are brought into R.
-file6 <- "TOIL-GTEX_TARGET_TCGA-ASSAY_DATA-LOG2TPM.tsv"
-rna <- data.table::fread(file=paste0(UCSC_Path, file6), check.names=FALSE, stringsAsFactors=F)
+    # TSV file of integrated RNAseq data are brought into R.
+    file6 <- "TOIL-GTEX_TARGET_TCGA-ASSAY_DATA-LOG2TPM.tsv"
+    rna <- data.table::fread(file=paste0(UCSC_Path, file6), check.names=FALSE, stringsAsFactors=F)
 
-# TXT file of membrane target candidates is brought into R.
-membrane <- data.table::fread("data/processed/membrane_target_candidates.csv")
+    # 2.Data analysis 
+    # Membrane antigen
+    membrane_targets <- membrane_targets[membrane_targets %in% gene$gene]
 
-# Data analysis 
-# Membrane antigen
-membrane <- membrane[, c("Gene ID", "Gene"), with = F]
-membrane <- membrane[Gene %in% gene$gene]
+    # UCSC Xena
+    # remove data from TARGET
+    meta <- meta[Study!="TARGET"]
+    meta <- meta[Primary.site!=""]
+    meta <- meta[Diagnosis!=""]
 
-# UCSC Xena
-# remove data from TARGET
-meta <- meta[Study!="TARGET"]
-meta <- meta[Primary.site!=""]
-meta <- meta[Diagnosis!=""]
+    # Thyroid
+    meta[Primary.site=="Thyroid Gland", Primary.site := "Thyroid"] 
 
-# Thyroid
-meta[Primary.site=="Thyroid Gland", Primary.site := "Thyroid"] 
+    # tissue type
+    meta[, type := fifelse(Diagnosis=="Normal", "normal", "cancer")]
 
-# tissue type
-meta[, type := fifelse(Diagnosis=="Normal", "normal", "cancer")]
+    # tissue.cancer
+    meta[, tissue.cancer := fifelse(Diagnosis=="Normal", Primary.site, Diagnosis)]
 
-# tissue.cancer
-meta[, tissue.cancer := fifelse(Diagnosis=="Normal", Primary.site, Diagnosis)]
+    # remove immunoprivileged tissue
+    # meta <- meta[Primary.site!="Testis"]
 
-# remove immunoprivileged tissue
-# meta <- meta[Primary.site!="Testis"]
+    # remove column
+    meta[, c("Primary.site", "Diagnosis", "Gender", "Study") := NULL]
 
-# remove column
-meta[, c("Primary.site", "Diagnosis", "Gender", "Study") := NULL]
+    # rna data
+    rna <- data.table::transpose(rna, keep.names = "Sample.ID")
 
-# rna data
-rna <- data.table::transpose(rna, keep.names = "Sample.ID")
+    data.table::setnames(rna, old=colnames(rna), new=c("Sample.ID", gene$gene))
 
-data.table::setnames(rna, old=colnames(rna), new=c("Sample.ID", gene$gene))
+    # select samples
+    rna <- rna[Sample.ID%in%meta$Sample.ID]
 
-# select samples
-rna <- rna[Sample.ID%in%meta$Sample.ID]
+    # select features
+    feature <- c("Sample.ID", membrane_targets)
+    rna <- rna[, ..feature]
+    rna <- merge(meta, rna, by="Sample.ID")
 
-# select features
-feature <- c("Sample.ID", membrane$Gene)
-rna <- rna[, ..feature]
-rna <- merge(meta, rna, by="Sample.ID")
-
-## Result output
-filename <- paste0("data/processed/TCGA_GTEX_integrated_membrane_target.txt")
-if(!exists(filename)){
-    data.table::fwrite(rna, file=filename)
+    # 3. results
+    return(rna)
 }
