1eda069ff6524af769581df17d65770da8272654
mbcxjrw4 <xiruowang@hotmail.com>
2025-07-31 14:25:28 +0100

    Create tissue_risk_categories.R

---
+++ config/tissue_risk_categories.R
@@ -0,0 +1,7 @@
+high <- c("Adipose Tissue", "Blood", "Blood Vessel", "Soft tissue/Bone", "Brain", "Esophagus", "Eye", "Heart", "Liver", "Lung", "Lymphatic tissue", "Muscle", "Nerve", "Paraganglia", "Small Intestine", "White blood cell")
+ 
+medium <- c("Adrenal Gland", "Bladder", "Colon", "Kidney", "Pancreas", "Pituitary", "Stomach", "Skin")
+
+low <- c("Breast", "Cervix", "Fallopian Tube", "Ovary", "Salivary Gland", "Prostate", "Rectum", "Spleen", "Thymus", "Thyroid", "Uterus", "Endometrium", "Vagina")
+
+immunoprivileged <- c("Testis")
