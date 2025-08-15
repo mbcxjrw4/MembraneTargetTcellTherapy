527c93fc7a0ff98ea5659bf196839370a8c8653a
mbcxjrw4 <xiruowang@hotmail.com>
2025-08-15 10:48:51 +0100

    Update tissue_risk_categories.R

--- config/tissue_risk_categories.R
+++ config/tissue_risk_categories.R
@@ -1,9 +1,7 @@
-high <- c("Adipose Tissue", "Blood", "Blood Vessel", "Soft tissue/Bone", "Brain", "Esophagus", "Eye", "Heart", "Liver", "Lung", "Lymphatic tissue", "Muscle", "Nerve", "Paraganglia", "Small Intestine", "White blood cell")
+high <- c("Adipose Tissue", "Blood", "Blood Vessel", "Soft tissue/Bone", "Brain", "Esophagus", "Eye", "Heart", "Liver", "Lung", "Lymphatic tissue", "Muscle", "Nerve", "Paraganglia", "Small Intestine", "White blood cell", "Bile duct", "Head and Neck region")
  
-medium <- c("Adrenal Gland", "Bladder", "Colon", "Kidney", "Pancreas", "Pituitary", "Stomach", "Skin")
+medium <- c("Adrenal Gland", "Bladder", "Colon", "Kidney", "Pancreas", "Pituitary", "Stomach", "Skin", "Lining of body cavities")
 
 low <- c("Breast", "Cervix", "Fallopian Tube", "Ovary", "Salivary Gland", "Prostate", "Rectum", "Spleen", "Thymus", "Thyroid", "Uterus", "Endometrium", "Vagina")
 
 immunoprivileged <- c("Testis")
-
-tbd <- c("Lining of body cavities", "Bile duct", "Head and Neck region")
